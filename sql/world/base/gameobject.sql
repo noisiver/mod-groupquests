322486db2b59e57f314460e4cfdda700ff13aa5e
Revision <tkn963@gmail.com>
2021-12-25 01:32:36 +0100

    Game object
    Added a game object to the module.
    
    Check the included changelog for more information.

--- sql/world/base/gameobject.sql
+++ sql/world/base/gameobject.sql
@@ -1,3 +1,7 @@
+-- Gri'lek the Wanderer (Quest: Kurzen's Mystery (Id: 207), Quest: The Vile Reef (Id: 629))
+-- Changed from 60 seconds to 1 second
+UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `id`=58;
+
 -- Alien Egg (Quest: Alien Egg (Id: 4821))
 -- Changed from 300 seconds to 1 second
 UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `id`=175565;
