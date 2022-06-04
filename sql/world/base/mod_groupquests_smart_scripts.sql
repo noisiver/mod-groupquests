e432907a2e72b129832d317af18c503831bc8ca9
Revision <tkn963@gmail.com>
2022-06-04 15:55:09 +0200

    Emergency Protocol: Section 8.2, Paragraph C

--- sql/world/base/mod_groupquests_smart_scripts.sql
+++ sql/world/base/mod_groupquests_smart_scripts.sql
@@ -1,2 +1,5 @@
+-- Fizzcrank Recon Pilot (Quest: Emergency Protocol: Section 8.2, Paragraph C (Id: 11795))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=25841 AND `id`=4;
+
 -- Wyrmcult Blackwhelp (Quest: Whelps of the Wyrmcult (Id: 10747))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2138700 AND `id`=2;
