efc196791ab6435ae315695ea9f55ed9298c9e63
Revision <tkn963@gmail.com>
2022-06-04 22:51:11 +0200

    Planning for the Future

--- sql/world/base/mod_groupquests_smart_scripts.sql
+++ sql/world/base/mod_groupquests_smart_scripts.sql
@@ -1,5 +1,8 @@
 -- Fizzcrank Recon Pilot (Quest: Emergency Protocol: Section 8.2, Paragraph C (Id: 11795))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=25841 AND `id`=4;
 
+-- Snowfall Glade Pup (Quest: Planning for the Future (Id: 11960))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26200 AND `id`=1;
+
 -- Wyrmcult Blackwhelp (Quest: Whelps of the Wyrmcult (Id: 10747))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2138700 AND `id`=2;
