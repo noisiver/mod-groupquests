8e3e8705c4bb8fbf19fe0192b521afb9c896247f
Revision <tkn963@gmail.com>
2022-06-07 12:38:01 +0200

    Securing the Bait

--- sql/world/base/mod_groupquests_smart_scripts.sql
+++ sql/world/base/mod_groupquests_smart_scripts.sql
@@ -7,5 +7,8 @@ UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26200 AND `id`=1
 -- Dead Mage Hunter (Quest: Rifle the Bodies (Id: 11999), Quest: Rifle the Bodies (Id: 12000))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26477 AND `id`=0;
 
+-- Primordial Drake Egg (Quest: Securing the Bait (Id: 12605))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=28408 AND `id`=2;
+
 -- Wyrmcult Blackwhelp (Quest: Whelps of the Wyrmcult (Id: 10747))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2138700 AND `id`=2;
