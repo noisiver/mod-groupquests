b1c46fe5aa9bdb5a2df8360483e113a844254233
Revision <tkn963@gmail.com>
2022-06-11 00:44:09 +0200

    The Drakkensryd
    The credit is multiplied for the members not killing the creature and I don't understand why so this will have to do for now.

--- sql/world/base/mod_groupquests_smart_scripts.sql
+++ sql/world/base/mod_groupquests_smart_scripts.sql
@@ -10,5 +10,8 @@ UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26477 AND `id`=0
 -- Primordial Drake Egg (Quest: Securing the Bait (Id: 12605))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=28408 AND `id`=2;
 
+-- Hyldsmeet Drakerider (Quest: The Drakkensryd (Id: 12886))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=29694 AND `id`=1;
+
 -- Wyrmcult Blackwhelp (Quest: Whelps of the Wyrmcult (Id: 10747))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2138700 AND `id`=2;
