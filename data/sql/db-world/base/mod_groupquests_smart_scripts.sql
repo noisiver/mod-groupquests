53e70f1357ef4a078952322b2de8a6941b2c0168
Revision <tkn963@gmail.com>
2023-02-18 18:58:35 +0100

    The Conquest Pit

--- data/sql/db-world/base/mod_groupquests_smart_scripts.sql
+++ data/sql/db-world/base/mod_groupquests_smart_scripts.sql
@@ -7,6 +7,21 @@ UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26200 AND `id`=1
 -- Dead Mage Hunter (Quest: Rifle the Bodies (Id: 11999), Quest: Rifle the Bodies (Id: 12000))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26477 AND `id`=0;
 
+-- Ironhide (Quest: The Conquest Pit: Bear Wrestling! (Id: 12427))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=27715 AND `id`=5;
+
+-- Torgg Thundertotem (Quest: The Conquest Pit: Mad Furbolg Fighting (Id: 12427))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=27716 AND `id`=6;
+
+-- Rustblood (Quest: The Conquest Pit: Blood and Metal (Id: 12429))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=27717 AND `id`=6;
+
+-- Horgrenn Hellcleave (Quest: The Conquest Pit: Death Is Likely (Id: 12430))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=27718 AND `id`=6;
+
+-- Conqueror Krenna (Quest: The Conquest Pit: Final Showdown (Id: 12431))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=27727 AND `id`=8;
+
 -- Primordial Drake Egg (Quest: Securing the Bait (Id: 12605))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=28408 AND `id`=2;
 
