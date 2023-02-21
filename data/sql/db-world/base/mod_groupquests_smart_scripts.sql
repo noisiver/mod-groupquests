b8fad7d60bec6de249e5e1f4ae9d0bef2426d6f4
Revision <tkn963@gmail.com>
2023-02-21 19:32:28 +0100

    Flown the Coop!, Chicken Party!

--- data/sql/db-world/base/mod_groupquests_smart_scripts.sql
+++ data/sql/db-world/base/mod_groupquests_smart_scripts.sql
@@ -30,3 +30,6 @@ UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=29694 AND `id`=1
 
 -- Wyrmcult Blackwhelp (Quest: Whelps of the Wyrmcult (Id: 10747))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2138700 AND `id`=2;
+
+-- Chicken Escapee (Quest: Flown the Coop! (Id: 12532), Quest: Chicken Party! (Id: 12702))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2816101 AND `id`=0;
