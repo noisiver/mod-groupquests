f1b38600aca445f0fc959058517bf6b7c81250bf
Revision <tkn963@gmail.com>
2023-05-19 13:43:54 +0200

    Elixir of Pain

--- data/sql/db-world/base/mod_groupquests_creature.sql
+++ data/sql/db-world/base/mod_groupquests_creature.sql
@@ -1,3 +1,7 @@
+-- Stanley (Quest: Elixir of Pain (Id: 502))
+-- Changed from 300 seconds to 1 second
+UPDATE `creature` SET `spawntimesecs`=1 WHERE `id1`=2274;
+
 -- Umpi (Quest: Elixir of Suffering (Id: 499))
 -- Changed from 300 seconds to 1 second
 UPDATE `creature` SET `spawntimesecs`=1 WHERE `id1`=2230;
