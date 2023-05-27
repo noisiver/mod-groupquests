f58969f35f5d3ed2fe9a0a43476b47bb3ecedf84
Revision <tkn963@gmail.com>
2023-05-27 15:43:31 +0200

    Emergency Supplies

--- data/sql/db-world/base/mod_groupquests_smart_scripts.sql
+++ data/sql/db-world/base/mod_groupquests_smart_scripts.sql
@@ -1,6 +1,9 @@
 -- Fizzcrank Recon Pilot (Quest: Emergency Protocol: Section 8.2, Paragraph C (Id: 11795))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=25841 AND `id`=4;
 
+-- Fizzcrank Recon Pilot (Quest: Emergency Supplies (Id: 11887))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=25841 AND `id`=1;
+
 -- Snowfall Glade Pup (Quest: Planning for the Future (Id: 11960))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=26200 AND `id`=1;
 
