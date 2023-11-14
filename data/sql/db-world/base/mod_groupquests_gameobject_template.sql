ea7a3134c0a905183a8bcb2b93d6bcd7e30b315b
Revision <tkn963@gmail.com>
2023-11-14 23:12:02 +0100

    Stones of Binding

--- data/sql/db-world/base/mod_groupquests_gameobject_template.sql
+++ data/sql/db-world/base/mod_groupquests_gameobject_template.sql
@@ -1,2 +1,11 @@
 -- Berard's Bookshelf (Quest: A Recipe For Death (Id: 450))
 UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=1594;
+
+-- Stone of West Binding (Quest: Stones of Binding (Id: 651))
+UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=2689;
+
+-- Stone of Outer Binding (Quest: Stones of Binding (Id: 651))
+UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=2690;
+
+-- Stone of East Binding (Quest: Stones of Binding (Id: 651))
+UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=2691;
