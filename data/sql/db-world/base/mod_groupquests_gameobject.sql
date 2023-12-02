6843652807b3f66bef4028a206e5bcf2a9ea269d
Revision <tkn963@gmail.com>
2023-12-02 18:24:51 +0100

    Kim'jael Indeed!

--- data/sql/db-world/base/mod_groupquests_gameobject.sql
+++ data/sql/db-world/base/mod_groupquests_gameobject.sql
@@ -70,6 +70,10 @@ UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `id`=148515;
 -- Changed from 180 seconds to 1 second
 UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `id`=148516;
 
+-- Kim'jael's Equipment (Quest: Kim'jael Indeed! (Id: 3601))
+-- Changed from 180 seconds to 1 second
+UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `id`=153123;
+
 -- Alien Egg (Quest: Alien Egg (Id: 4821))
 -- Changed from 300 seconds to 1 second
 UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `id`=175565;
