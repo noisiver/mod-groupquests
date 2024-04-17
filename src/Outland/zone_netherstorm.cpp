469b799e2c45cac3578469d2edab6b96359fe4aa
Revision <tkn963@gmail.com>
2024-04-17 19:26:01 +0200

    Information Gathering

--- src/Outland/zone_netherstorm.cpp
+++ src/Outland/zone_netherstorm.cpp
@@ -148,7 +148,309 @@ public:
     }
 };
 
+enum CommanderDawnforgeData
+{
+    SAY_COMMANDER_DAWNFORGE_1 = 0,
+    SAY_COMMANDER_DAWNFORGE_2 = 1,
+    SAY_COMMANDER_DAWNFORGE_3 = 2,
+    SAY_COMMANDER_DAWNFORGE_4 = 3,
+    SAY_COMMANDER_DAWNFORGE_5 = 4,
+
+    SAY_ARCANIST_ARDONIS_1 = 0,
+    SAY_ARCANIST_ARDONIS_2 = 1,
+
+    SAY_PATHALEON_CULATOR_IMAGE_1 = 0,
+    SAY_PATHALEON_CULATOR_IMAGE_2 = 1,
+    SAY_PATHALEON_CULATOR_IMAGE_2_1 = 2,
+    SAY_PATHALEON_CULATOR_IMAGE_2_2 = 3,
+
+    QUEST_INFO_GATHERING = 10198,
+    SPELL_SUNFURY_DISGUISE = 34603,
+};
+
+// Entries of Arcanist Ardonis, Commander Dawnforge, Pathaleon the Curators Image
+const uint32 CreatureEntry[3] =
+{
+    19830,                                                // Ardonis
+    19831,                                                // Dawnforge
+    21504                                                 // Pathaleon
+};
+
+class npc_commander_dawnforge_groupquests : public CreatureScript
+{
+public:
+    npc_commander_dawnforge_groupquests() : CreatureScript("npc_commander_dawnforge") { }
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new npc_commander_dawnforge_groupquestsAI(creature);
+    }
+
+    struct npc_commander_dawnforge_groupquestsAI : public ScriptedAI
+    {
+        npc_commander_dawnforge_groupquestsAI(Creature* creature) : ScriptedAI(creature) { }
+
+        ObjectGuid PlayerGUID;
+        ObjectGuid ardonisGUID;
+        ObjectGuid pathaleonGUID;
+
+        uint32 Phase;
+        uint32 PhaseSubphase;
+        uint32 Phase_Timer;
+        bool isEvent;
+
+        void Reset() override
+        {
+            PlayerGUID.Clear();
+            ardonisGUID.Clear();
+            pathaleonGUID.Clear();
+
+            Phase = 1;
+            PhaseSubphase = 0;
+            Phase_Timer = 4000;
+            isEvent = false;
+        }
+
+        void JustEngagedWith(Unit* /*who*/) override { }
+
+        void JustSummoned(Creature* summoned) override
+        {
+            pathaleonGUID = summoned->GetGUID();
+        }
+
+        // Emote Ardonis and Pathaleon
+        void Turn_to_Pathaleons_Image()
+        {
+            Creature* ardonis = ObjectAccessor::GetCreature(*me, ardonisGUID);
+            Creature* pathaleon = ObjectAccessor::GetCreature(*me, pathaleonGUID);
+
+            if (!ardonis || !pathaleon)
+                return;
+
+            // Turn Dawnforge
+            me->SetFacingToObject(pathaleon);
+
+            // Turn Ardonis
+            ardonis->SetFacingToObject(pathaleon);
+
+            //Set them to kneel
+            me->SetStandState(UNIT_STAND_STATE_KNEEL);
+            ardonis->SetStandState(UNIT_STAND_STATE_KNEEL);
+        }
+
+        //Set them back to each other
+        void Turn_to_eachother()
+        {
+            if (Unit* ardonis = ObjectAccessor::GetUnit(*me, ardonisGUID))
+            {
+                // Turn Dawnforge
+                me->SetFacingToObject(ardonis);
+
+                // Turn Ardonis
+                ardonis->SetFacingToObject(me);
+
+                //Set state
+                me->SetStandState(UNIT_STAND_STATE_STAND);
+                ardonis->SetStandState(UNIT_STAND_STATE_STAND);
+            }
+        }
+
+        bool CanStartEvent(Player* player)
+        {
+            if (!isEvent)
+            {
+                Creature* ardonis = me->FindNearestCreature(CreatureEntry[0], 10.0f);
+                if (!ardonis)
+                    return false;
+
+                ardonisGUID = ardonis->GetGUID();
+                PlayerGUID = player->GetGUID();
+
+                isEvent = true;
+
+                Turn_to_eachother();
+                return true;
+            }
+
+            LOG_DEBUG("scripts.ai", "npc_commander_dawnforge event already in progress, need to wait.");
+            return false;
+        }
+
+        void UpdateAI(uint32 diff) override
+        {
+            //Is event even running?
+            if (!isEvent)
+                return;
+
+            //Phase timing
+            if (Phase_Timer >= diff)
+            {
+                Phase_Timer -= diff;
+                return;
+            }
+
+            Creature* ardonis = ObjectAccessor::GetCreature(*me, ardonisGUID);
+            Creature* pathaleon = ObjectAccessor::GetCreature(*me, pathaleonGUID);
+            Player* player = ObjectAccessor::GetPlayer(*me, PlayerGUID);
+
+            if (!ardonis || !player)
+            {
+                Reset();
+                return;
+            }
+
+            if (Phase > 4 && !pathaleon)
+            {
+                Reset();
+                return;
+            }
+
+            //Phase 1 Dawnforge say
+            switch (Phase)
+            {
+            case 1:
+                Talk(SAY_COMMANDER_DAWNFORGE_1);
+                ++Phase;
+                Phase_Timer = 16000;
+                break;
+                //Phase 2 Ardonis say
+            case 2:
+                ardonis->AI()->Talk(SAY_ARCANIST_ARDONIS_1);
+                ++Phase;
+                Phase_Timer = 16000;
+                break;
+                //Phase 3 Dawnforge say
+            case 3:
+                Talk(SAY_COMMANDER_DAWNFORGE_2);
+                ++Phase;
+                Phase_Timer = 16000;
+                break;
+                //Phase 4 Pathaleon spawns up to phase 9
+            case 4:
+                //spawn pathaleon's image
+                me->SummonCreature(CreatureEntry[2], 2325.851563f, 2799.534668f, 133.084229f, 6.038996f, TEMPSUMMON_TIMED_DESPAWN, 90000);
+                ++Phase;
+                Phase_Timer = 500;
+                break;
+                //Phase 5 Pathaleon say
+            case 5:
+                pathaleon->AI()->Talk(SAY_PATHALEON_CULATOR_IMAGE_1);
+                ++Phase;
+                Phase_Timer = 6000;
+                break;
+                //Phase 6
+            case 6:
+                switch (PhaseSubphase)
+                {
+                    //Subphase 1: Turn Dawnforge and Ardonis
+                case 0:
+                    Turn_to_Pathaleons_Image();
+                    ++PhaseSubphase;
+                    Phase_Timer = 8000;
+                    break;
+                    //Subphase 2 Dawnforge say
+                case 1:
+                    Talk(SAY_COMMANDER_DAWNFORGE_3);
+                    PhaseSubphase = 0;
+                    ++Phase;
+                    Phase_Timer = 8000;
+                    break;
+                }
+                break;
+                //Phase 7 Pathaleons say 3 Sentence, every sentence need a subphase
+            case 7:
+                switch (PhaseSubphase)
+                {
+                    //Subphase 1
+                case 0:
+                    pathaleon->AI()->Talk(SAY_PATHALEON_CULATOR_IMAGE_2);
+                    ++PhaseSubphase;
+                    Phase_Timer = 12000;
+                    break;
+                    //Subphase 2
+                case 1:
+                    pathaleon->AI()->Talk(SAY_PATHALEON_CULATOR_IMAGE_2_1);
+                    ++PhaseSubphase;
+                    Phase_Timer = 16000;
+                    break;
+                    //Subphase 3
+                case 2:
+                    pathaleon->AI()->Talk(SAY_PATHALEON_CULATOR_IMAGE_2_2);
+                    PhaseSubphase = 0;
+                    ++Phase;
+                    Phase_Timer = 10000;
+                    break;
+                }
+                break;
+                //Phase 8 Dawnforge & Ardonis say
+            case 8:
+                Talk(SAY_COMMANDER_DAWNFORGE_4);
+                ardonis->AI()->Talk(SAY_ARCANIST_ARDONIS_2);
+                ++Phase;
+                Phase_Timer = 4000;
+                break;
+                //Phase 9 Pathaleons Despawn, Reset Dawnforge & Ardonis angle
+            case 9:
+                Turn_to_eachother();
+                //hide pathaleon, unit will despawn shortly
+                pathaleon->SetVisible(false);
+                PhaseSubphase = 0;
+                ++Phase;
+                Phase_Timer = 3000;
+                break;
+                //Phase 10 Dawnforge say
+            case 10:
+                Talk(SAY_COMMANDER_DAWNFORGE_5);
+
+                if (Group* group = player->GetGroup())
+                {
+                    group->DoForAllMembers([player](Player* member)
+                    {
+                        if (member->IsAtGroupRewardDistance(player))
+                        {
+                            member->AreaExploredOrEventHappens(QUEST_INFO_GATHERING);
+                        }
+                    });
+                }
+                else
+                {
+                    player->AreaExploredOrEventHappens(QUEST_INFO_GATHERING);
+                }
+
+                Reset();
+                break;
+            }
+        }
+    };
+};
+
+class at_commander_dawnforge_groupquests : public AreaTriggerScript
+{
+public:
+    at_commander_dawnforge_groupquests() : AreaTriggerScript("at_commander_dawnforge") { }
+
+    bool OnTrigger(Player* player, const AreaTrigger* /*at*/) override
+    {
+        //if player lost aura or not have at all, we should not try start event.
+        if (!player->HasAura(SPELL_SUNFURY_DISGUISE))
+            return false;
+
+        if (player->IsAlive() && player->GetQuestStatus(QUEST_INFO_GATHERING) == QUEST_STATUS_INCOMPLETE)
+        {
+            Creature* Dawnforge = player->FindNearestCreature(CreatureEntry[1], 30.0f);
+            if (!Dawnforge)
+                return false;
+
+            if (CAST_AI(npc_commander_dawnforge_groupquests::npc_commander_dawnforge_groupquestsAI, Dawnforge->AI())->CanStartEvent(player))
+                return true;
+        }
+        return false;
+    }
+};
+
 void AddSC_zone_netherstorm_groupquests()
 {
     new npc_maxx_a_million_escort_groupquests();
+    new npc_commander_dawnforge_groupquests();
+    new at_commander_dawnforge_groupquests();
 }
