ed797dfaca9c9bc380a67cf1f106f311b104ae2b
Revision <tkn963@gmail.com>
2024-04-19 06:49:25 +0200

    Forgotten Treasure

--- data/sql/db-world/base/mod_groupquests_gameobject_template.sql
+++ data/sql/db-world/base/mod_groupquests_gameobject_template.sql
@@ -13,8 +13,11 @@ UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=2691;
 -- Ikeyen's Belongings (Quest: A Damp, Dark Place (Id: 9788))
 UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=182122;
 
+-- Sealed Box (Quest: Investigate Tuurem (Id: 9990))
+UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=182542;
+
 -- Suspicious Outhouse (Quest: Capturing the Phylactery (Id: 10319))
 UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=184310;
 
--- Sealed Box (Quest: Investigate Tuurem (Id: 9990))
-UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=182542;
+-- Eagle Figurine (Quest: Forgotten Treasure (Id: 11434))
+UPDATE `gameobject_template` SET `Data2`=0 WHERE `entry`=186886;
