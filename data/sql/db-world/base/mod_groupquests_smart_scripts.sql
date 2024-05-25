9b4cf3bf12a5e5bdaf257fd530f03f65f2adee7b
Revision <tkn963@gmail.com>
2024-05-25 16:30:07 +0200

    The Aspirant's Challenge

--- data/sql/db-world/base/mod_groupquests_smart_scripts.sql
+++ data/sql/db-world/base/mod_groupquests_smart_scripts.sql
@@ -46,6 +46,9 @@ UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=28408 AND `id`=2
 -- Hyldsmeet Drakerider (Quest: The Drakkensryd (Id: 12886))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=29694 AND `id`=1;
 
+-- Argent Valiant (Quest: The Aspirant's Challenge (Id: 13679), Quest: The Aspirant's Challenge (Id: 13680))
+UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=33448 AND `id`=16;
+
 -- Wyrmcult Blackwhelp (Quest: Whelps of the Wyrmcult (Id: 10747))
 UPDATE `smart_scripts` SET `target_type`=16 WHERE `entryorguid`=2138700 AND `id`=2;
 
